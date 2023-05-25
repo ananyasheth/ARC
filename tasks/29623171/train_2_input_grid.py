dbd57610e8e37f8bda544b55fce9c86cc70212cd
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-05-25 15:59:53 -0400

    Update train_2_input_grid.py

--- tasks/29623171/train_2_input_grid.py
+++ tasks/29623171/train_2_input_grid.py
@@ -5,6 +5,21 @@ from GroupOfShapes import *
 
 def input_grid():
 	return(GroupOfShapes([
-			Background((7,9)),
-			Line(((2,0),(2,3)),SolidFill(2))
+			Background((11,11)),
+			Line(((0,3),(10,3)),SolidFill(5)),
+                        Line(((0,7),(10,7)),SolidFill(5)),
+                        Line(((3,0),(3,10)),SolidFill(5)),
+                        Line(((7,0),(7,10)),SolidFill(5)),
+                        Dot((0,0),SolidFill(3)),
+	                Dot((0,1),SolidFill(3)),
+		        Dot((1,9),SolidFill(3)),
+		        Dot((5,1),SolidFill(3)),
+			Dot((5,5),SolidFill(3)),
+			Dot((6,4),SolidFill(3)),
+			Dot((9,1),SolidFill(3)),
+			Dot((9,4),SolidFill(3)),
+			Dot((9,8),SolidFill(3)),
+			Dot((9,9),SolidFill(3)),
+			Dot((10,10),SolidFill(3))
 			]))
+

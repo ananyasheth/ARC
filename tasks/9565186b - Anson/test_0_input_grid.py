c1410c47711d2c27df5c008127d9e66b78f90633
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-05-29 16:43:16 -0400

    Update test_0_input_grid.py

--- tasks/9565186b - Anson/test_0_input_grid.py
+++ tasks/9565186b - Anson/test_0_input_grid.py
@@ -1,1 +1,14 @@
+from ImageMatrix import *
+from Shape import *
+from Fill import *
+from GroupOfShapes import *
 
+def input_grid():
+        return(GroupOfShapes([
+                Background((3,3)),
+                Dot((0,0),SolidFill(1)),
+                Dot((1,0),SolidFill(3)),
+                Dot((2,0),SolidFill(1)),
+                Line(((0,1),(2,1)),SolidFill(3)),
+                Line(((0,2),(2,2)),SolidFill(2))
+                ]))

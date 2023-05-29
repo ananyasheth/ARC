bae1b8df544b4d5894069fb40b40b81ed44aa2a0
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-05-29 17:59:52 -0400

    Update transform.py

--- tasks/9565186b/transform.py
+++ tasks/9565186b/transform.py
@@ -4,16 +4,7 @@ from Fill import *
 from GroupOfShapes import *
 
 def transform(group_of_shapes):
-
-    new_group_of_shapes=[]
-    new_group_of_shapes.append(group_of_shapes)
-    mainline = group_of_shapes.fetch_shape(['HLine'])
-    for line_number in range(0,mainline.min_x+mainline.y_size):
-      if line_number>=mainline.min_x:
-        if line_number==mainline.min_x:
-          continue
-        else:
-          new_group_of_shapes.append(Line(((line_number,mainline.min_y),(line_number,mainline.min_x+mainline.y_size-line_number-1)),SolidFill(1)))
-      else:
-        new_group_of_shapes.append(Line(((line_number,mainline.min_y),(line_number,mainline.min_x+mainline.y_size-line_number-1)),SolidFill(3)))
-    return(GroupOfShapes(new_group_of_shapes))
+	largest_rectangle = group_of_shapes.largest('RectangleOutline')
+	return(GroupOfShapes([
+			Background((2,2),SolidFill(largest_rectangle.fill.colour))
+			]))

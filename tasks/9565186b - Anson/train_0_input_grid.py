02f17d8e8103684441156e31c04d3db316fbd42f
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-05-29 16:36:04 -0400

    Update train_0_input_grid.py

--- tasks/9565186b - Anson/train_0_input_grid.py
+++ tasks/9565186b - Anson/train_0_input_grid.py
@@ -5,10 +5,10 @@ from GroupOfShapes import *
 
 def input_grid():
 	return(GroupOfShapes([
-    Background((3,3)),
-    Line(((0,0),(0,2)),SolidFill(2)),
-    Line(((0,0),(2,0)),SolidFill(2)),
-    Dot((1,0),SolidFill(1)),
-    Line(((1,2),(2,2)),SolidFill(8)),
-    Line(((2,1),(2,2)),SolidFill(8))
-  ]))
+		Background((3,3)),
+		Line(((0,0),(0,2)),SolidFill(2)),
+		Line(((0,0),(2,0)),SolidFill(2)),
+		Dot((1,0),SolidFill(1)),
+		Line(((1,2),(2,2)),SolidFill(8)),
+		Line(((2,1),(2,2)),SolidFill(8))
+		]))

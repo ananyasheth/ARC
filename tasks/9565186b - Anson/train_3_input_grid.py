268d92176f9e3cee2e5bd99ca478e4ee6c02c670
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-05-29 16:34:13 -0400

    Update train_3_input_grid.py

--- tasks/9565186b - Anson/train_3_input_grid.py
+++ tasks/9565186b - Anson/train_3_input_grid.py
@@ -1,1 +1,14 @@
+from ImageMatrix import *
+from Shape import *
+from Fill import *
+from GroupOfShapes import *
 
+def input_grid():
+	return(GroupOfShapes([
+    Background((3,3)),
+    Line(((0,0),(0,1)),SolidFill(3)),
+    Line(((1,0),(1,2)),SolidFill(4)),
+    Dot((0,2),SolidFill(8)),
+    Dot((2,0),SolidFill(8)),
+    Line(((2,1),(2,2)),SolidFill(1))
+  ]))

8174c3e46f3638209442cfa4930b95d048c618b7
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-05-31 17:01:54 -0400

    Update transform.py

--- tasks/67a423a3/transform.py
+++ tasks/67a423a3/transform.py
@@ -5,7 +5,8 @@ from GroupOfShapes import *
 
 def transform(group_of_shapes):
     lines = group_of_shapes.fetch_shape(['HLine','VLine','DLine'])
-    point1,point2 = group_of_shapes.intersection_of_lines(lines)
+    list_of_points = group_of_shapes.intersection_of_lines(lines)
+    if len(list_of_points)==1:
+      point1,point2=list_of_points[0][0],list_of_points[0][1]
     new_rectangle = RectangleOutline(((point1-1,point2-1),(point1+1,point2+1)),SolidFill(4))
     return(GroupOfShapes([group_of_shapes,new_rectangle]))
-	

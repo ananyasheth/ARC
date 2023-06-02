a0353d28bb6ce69fc3e1efbcece84516a0d532cb
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-06-02 15:07:54 -0400

    Update transform.py

--- tasks/29623171/transform.py
+++ tasks/29623171/transform.py
@@ -4,16 +4,23 @@ from Fill import *
 from GroupOfShapes import *
 
 def transform(group_of_shapes):
-
-    new_group_of_shapes=[]
-    new_group_of_shapes.append(group_of_shapes)
-    mainline = group_of_shapes.fetch_shape(['HLine'])
-    for line_number in range(0,mainline.min_x+mainline.y_size):
-      if line_number>=mainline.min_x:
-        if line_number==mainline.min_x:
-          continue
-        else:
-          new_group_of_shapes.append(Line(((line_number,mainline.min_y),(line_number,mainline.min_x+mainline.y_size-line_number-1)),SolidFill(1)))
-      else:
-        new_group_of_shapes.append(Line(((line_number,mainline.min_y),(line_number,mainline.min_x+mainline.y_size-line_number-1)),SolidFill(3)))
-    return(GroupOfShapes(new_group_of_shapes))
+  number_of_dots=[]
+  background = group_of_shapes.fetch_shape(['Background'])
+  lines = group_of_shapes.fetch_shape(['HLine','VLine'])
+  dots = group_of_shapes.fetch_shape(['Dot'])
+  intersection_points = group_of_shapes.intersection_of_lines(lines)
+  unique_segments = group_of_shapes.segment_grid(intersection_points,3)
+  for each_segment in unique_segments:
+    dots_in_each_segment = group_of_shapes.fetch_shape(['Dot'],each_segment)
+    if type(dots_in_each_segment)==list:
+      number_of_dots.append(len(dots_in_each_segment))
+    else:
+      number_of_dots.append(1)
+  max_value = max(number_of_dots)
+  max_indices = [index for index, value in enumerate(number_of_dots) if value == max_value]
+  new_group_of_shapes = [background]
+  for each_line in lines:
+    new_group_of_shapes.append(each_line)
+  for each_index in max_indices:
+    new_group_of_shapes.append(Rectangle(unique_segments[each_index],SolidFill(dots[0].fill.colour)))
+  return GroupOfShapes(new_group_of_shapes)

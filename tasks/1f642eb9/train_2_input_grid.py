5a0c549bfc7b6335448bea53ab15b4dfff397552
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-06-20 17:35:19 -0400

    Update train_2_input_grid.py

--- tasks/1f642eb9/train_2_input_grid.py
+++ tasks/1f642eb9/train_2_input_grid.py
@@ -1,1 +1,12 @@
+from ImageMatrix import *
+from Shape import *
+from Fill import *
+from GroupOfShapes import *
+
+def input_grid():
+	return(GroupOfShapes([
+			Background((10,10)),
+			RectangleOutline(((0,1),(6,6)),SolidFill(4)),
+			RectangleOutline(((7,7),(9,9)),SolidFill(2))
+			]))
 

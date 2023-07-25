1c05c443402957cea8c265ed18259b1967586df1
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-07-24 19:43:22 -0500

    Update transform.py

--- tasks/508bd3b6/transform.py
+++ tasks/508bd3b6/transform.py
@@ -13,6 +13,11 @@ def transform(group_of_shapes):
     hlines = group_of_shapes.fetch_shape(['HLine'])
     vlines = group_of_shapes.fetch_shape(['VLine'])
     
+    def add_bounce():
+      lines = group_of_shapes.bounce((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1),(new_x2_l2,new_y2_l2),3)
+      for line in lines:
+        group_of_shapes.add_on_top(line)
+    
     if hlines == []:            # Vertical Line
       min_y = max_y = None
       for each_line in vlines:
@@ -45,13 +50,13 @@ def transform(group_of_shapes):
         while(new_y2_l1!=min_y-1):
           new_x2_l1+=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.max_x):
           new_x2_l2+=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
+        add_bounce()
+        
       
       if hlines == [] and max_y<int(background.y_size/2):      # Vertical Line on the left
         new_x1_l1=new_x2_l1=x1-1
@@ -59,13 +64,12 @@ def transform(group_of_shapes):
         while(new_y2_l1!=max_y+1):
           new_x2_l1-=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.min_x):
           new_x2_l2-=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
+        add_bounce()
       
       if vlines == [] and max_x>int(background.x_size/2):      # Horizontal Line on the bottom
         new_x1_l1=new_x2_l1=x2+1
@@ -73,27 +77,25 @@ def transform(group_of_shapes):
         while(new_x2_l1!=min_x-1):
           new_x2_l1+=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.max_y):
           new_x2_l2-=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
+        
       if vlines == [] and max_x<int(background.x_size/2):      # Horizontal Line on the top
         new_x1_l1=new_x2_l1=x1-1
         new_y1_l1=new_y2_l1=y1-1
         while(new_x2_l1!=max_x+1):
           new_x2_l1-=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.min_y):
           new_x2_l2+=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
+        add_bounce()
 
 
     if x1 < x2 and y1 > y2:   # Diagonal Down Left
@@ -103,56 +105,51 @@ def transform(group_of_shapes):
         while(new_y2_l1!=min_y-1):
           new_x2_l1-=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.min_x):
           new_x2_l2-=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if hlines == [] and max_y<int(background.y_size/2):      # Vertical Line on the left
         new_x1_l1=new_x2_l1=x2+1
         new_y1_l1=new_y2_l1=y2-1
         while(new_y2_l1!=max_y+1):
           new_x2_l1+=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.max_x):
           new_x2_l2+=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if vlines == [] and max_x>int(background.x_size/2):      # Horizontal Line on the bottom
         new_x1_l1=new_x2_l1=x2+1
         new_y1_l1=new_y2_l1=y2-1
         while(new_x2_l1!=min_x-1):
           new_x2_l1+=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.min_y):
           new_x2_l2-=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
+        
       if vlines == [] and max_x<int(background.x_size/2):      # Horizontal Line on the top
         new_x1_l1=new_x2_l1=x1-1
         new_y1_l1=new_y2_l1=y1+1
         while(new_x2_l1!=max_x+1):
           new_x2_l1-=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.max_y):
           new_x2_l2+=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
 
     if x1 > x2 and y1 < y2:      # Diagonal Up Right
       if hlines == [] and max_y>int(background.y_size/2):     # Vertical Line on the right
@@ -161,56 +158,51 @@ def transform(group_of_shapes):
         while(new_y2_l1!=min_y-1):
           new_x2_l1-=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.min_x):
           new_x2_l2-=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if hlines == [] and max_y<int(background.y_size/2):      # Vertical Line on the left
         new_x1_l1=new_x2_l1=x1+1
         new_y1_l1=new_y2_l1=y1-1
         while(new_y2_l1!=max_y+1):
           new_x2_l1+=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.max_x):
           new_x2_l2+=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if vlines == [] and max_x>int(background.x_size/2):      # Horizontal Line on the bottom
         new_x1_l1=new_x2_l1=x1+1
         new_y1_l1=new_y2_l1=y1-1
         while(new_x2_l1!=min_x-1):
           new_x2_l1+=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.min_y):
           new_x2_l2-=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
+        
       if vlines == [] and max_x<int(background.x_size/2):      # Horizontal Line on the top
         new_x1_l1=new_x2_l1=x2-1
         new_y1_l1=new_y2_l1=y2+1
         while(new_x2_l1!=max_x+1):
           new_x2_l1-=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.max_y):
           new_x2_l2+=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
  
     if x1 > x2 and y1 > y2:      # Diagonal Up left
       if hlines == [] and max_y>int(background.y_size/2):     # Vertical Line on the right
@@ -219,55 +211,50 @@ def transform(group_of_shapes):
         while(new_y2_l1!=min_y-1):
           new_x2_l1+=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.max_x):
           new_x2_l2+=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if hlines == [] and max_y<int(background.y_size/2):      # Vertical Line on the left
         new_x1_l1=new_x2_l1=x2-1
         new_y1_l1=new_y2_l1=y2-1
         while(new_y2_l1!=max_y+1):
           new_x2_l1-=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_x2_l2!=background.min_x):
           new_x2_l2-=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-      
+        add_bounce()
+        
       if vlines == [] and max_x>int(background.x_size/2):      # Horizontal Line on the bottom
         new_x1_l1=new_x2_l1=x1+1
         new_y1_l1=new_y2_l1=y1+1
         while(new_x2_l1!=min_x-1):
           new_x2_l1+=1
           new_y2_l1+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.max_y):
           new_x2_l2-=1
           new_y2_l2+=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
+        
       if vlines == [] and max_x<int(background.x_size/2):      # Horizontal Line on the top
         new_x1_l1=new_x2_l1=x2-1
         new_y1_l1=new_y2_l1=y2-1
         while(new_x2_l1!=max_x+1):
           new_x2_l1-=1
           new_y2_l1-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l1,new_y1_l1),(new_x2_l1,new_y2_l1)),SolidFill(3)))
         new_x1_l2=new_x2_l2=new_x2_l1
         new_y1_l2=new_y2_l2=new_y2_l1
         while(new_y2_l2!=background.min_y):
           new_x2_l2+=1
           new_y2_l2-=1
-        group_of_shapes.add_on_top(Line(((new_x1_l2,new_y1_l2),(new_x2_l2,new_y2_l2)),SolidFill(3)))
-
+        add_bounce()
         
     return (GroupOfShapes([group_of_shapes]))

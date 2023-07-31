15f369d7f8612eaa6a9fac890452c3123d9d1e57
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-07-31 18:47:06 -0400

    upload level

---
+++ level.py
@@ -0,0 +1,37 @@
+'''
+The difficulty of the tasks
+'''
+
+'''
+Task with bugs
+1f642eb9
+'''
+level={
+  'Hard':[
+    '508bd3b6',
+    '29623171',
+    'a65b410d',
+    'dae9d2b5'
+  ],
+  'Medium':[
+    '6d58a25d',
+    '025d127b',
+    '67a423a3',
+    'a85d4709',
+    'aba27056',
+    'd4f3cd78',
+    'e9afcf9a'
+    
+  ],
+  'Easy':[
+    '6f8cd79b',
+    '25d487eb',
+    '445eab21',
+    '623ea044',
+    '95990924',
+    'd037b0a7',
+    'dc1df850',
+    '56ff96f3',
+    'a2fd1cf0',
+  ]
+}

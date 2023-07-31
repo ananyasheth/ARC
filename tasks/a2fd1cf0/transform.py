e9fe0278a789da0728a13aceea18d4eeb2c875b1
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-07-31 18:55:28 -0400

    Update transform.py

--- tasks/a2fd1cf0/transform.py
+++ tasks/a2fd1cf0/transform.py
@@ -9,30 +9,23 @@ def transform(group_of_shapes):
     dots = group_of_shapes.fetch_shape(['Dot'])
 
     if dots[0].fill.colour == 2 :
-        if dots[0].min_x < dots[1].min_x and dots[0].min_y < dots[1].min_y:     #
-            polyline = Polyline(((dots[0].min_x, dots[0].min_y+1),(dots[0].min_x, dots[1].min_y),(dots[1].min_x-1, dots[1].min_y)),SolidFill(8))
-
+        if dots[0].min_x < dots[1].min_x and dots[0].min_y < dots[1].min_y:
+            polyline = group_of_shapes.join((dots[0].min_x,dots[0].min_y+1),(dots[1].min_x-1,dots[1].min_y),'right-angle',8)
         elif dots[0].min_x < dots[1].min_x and dots[0].min_y > dots[1].min_y:
-            polyline = Polyline(((dots[0].min_x, dots[0].min_y-1),(dots[0].min_x, dots[1].min_y),(dots[1].min_x-1, dots[1].min_y)),SolidFill(8))
-
+            polyline = group_of_shapes.join((dots[0].min_x,dots[0].min_y-1),(dots[1].min_x-1,dots[1].min_y),'left-angle',8)
         elif dots[0].min_x > dots[1].min_x and dots[0].min_y < dots[1].min_y:
-            polyline = Polyline(((dots[0].min_x, dots[0].min_y+1),(dots[0].min_x, dots[1].min_y),(dots[1].min_x+1, dots[1].min_y)),SolidFill(8))
-
+            polyline = group_of_shapes.join((dots[0].min_x,dots[0].min_y+1),(dots[1].min_x+1,dots[1].min_y),'right-angle',8)
         else:
-            polyline = Polyline(((dots[0].min_x, dots[0].min_y-1),(dots[0].min_x, dots[1].min_y),(dots[1].min_x+1, dots[1].min_y)),SolidFill(8))
-
+            polyline = group_of_shapes.join((dots[0].min_x,dots[0].min_y-1),(dots[1].min_x+1,dots[1].min_y),'left-angle',8)
     else:
-        if dots[1].min_x < dots[0].min_x and dots[1].min_y < dots[0].min_y:
-            polyline = Polyline(((dots[1].min_x, dots[1].min_y+1),(dots[1].min_x, dots[0].min_y),(dots[0].min_x-1, dots[0].min_y)),SolidFill(8))
-
-        elif dots[1].min_x < dots[0].min_x and dots[1].min_y > dots[0].min_y:
-                polyline = Polyline(((dots[1].min_x, dots[1].min_y-1),(dots[1].min_x, dots[0].min_y),(dots[0].min_x-1, dots[0].min_y)),SolidFill(8))
-
-        elif dots[1].min_x > dots[0].min_x and dots[1].min_y < dots[0].min_y:
-            polyline = Polyline(((dots[1].min_x, dots[1].min_y+1),(dots[1].min_x, dots[0].min_y),(dots[0].min_x+1, dots[0].min_y)),SolidFill(8))
-
+        if dots[0].min_x < dots[1].min_x and dots[0].min_y < dots[1].min_y:
+            polyline = group_of_shapes.join((dots[0].min_x+1,dots[0].min_y),(dots[1].min_x,dots[1].min_y-1),'left-angle',8)
+        elif dots[0].min_x < dots[1].min_x and dots[0].min_y > dots[1].min_y:
+            polyline = group_of_shapes.join((dots[0].min_x-1,dots[0].min_y),(dots[1].min_x,dots[1].min_y-1),'right-angle',8)
+        elif dots[0].min_x > dots[1].min_x and dots[0].min_y < dots[1].min_y:
+            polyline = group_of_shapes.join((dots[0].min_x+1,dots[0].min_y),(dots[1].min_x,dots[1].min_y+1),'left-angle',8)
         else:
-            polyline = Polyline(((dots[1].min_x, dots[1].min_y-1),(dots[1].min_x, dots[0].min_y),(dots[0].min_x+1, dots[0].min_y)),SolidFill(8))
+            polyline = group_of_shapes.join((dots[0].min_x-1,dots[0].min_y),(dots[1].min_x,dots[1].min_y+1),'right-angle',8)
 
     new_group_of_shapes.append(polyline)
     

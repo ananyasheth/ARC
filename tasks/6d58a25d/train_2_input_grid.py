6747ba26bfc0b70ee916218c9540cea62c31c918
Anson Chan <113957693+Anson3208@users.noreply.github.com>
2023-08-05 01:29:53 -0400

    Update train_2_input_grid.py

--- tasks/6d58a25d/train_2_input_grid.py
+++ tasks/6d58a25d/train_2_input_grid.py
@@ -5,7 +5,7 @@ from GroupOfShapes import *
 
 def input_grid():
     triangle = Triangle(((7,9),(4,12),(7,15)),SolidFill(4))
-    tshape = Tshape(((7,10),(7,14),(6,12)),SolidFill(0))
+    tshape = Tshape(((7,10),(7,14),(6,12),(6,12)),SolidFill(0))
     triangle.delete(tshape)
     return(GroupOfShapes([
         Background((20,20)),

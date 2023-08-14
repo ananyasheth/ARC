e00d0226099891feb7e1ba754bbfda6e830640fb
Ananya Sheth <67687880+ananyasheth@users.noreply.github.com>
2023-08-14 11:38:07 -0400

    Update driver_code.py

--- DQL/driver_code.py
+++ DQL/driver_code.py
@@ -1,18 +1,4 @@
 """
-Deep Learning Reinforcement Tutorial: Deep Q Network (DQN) = Combination of Deep Learning and Q-Learning Tutorial
-
-This file contains driver code that imports DeepQLearning class developed in the file "functions_final"
- 
-The class DeepQLearning implements the Deep Q Network (DQN) Reinforcement Learning Algorithm.
-The implementation is based on the OpenAI Gym Cart Pole environment and TensorFlow (Keras) machine learning library
-
-The webpage explaining the codes and the main idea of the DQN is given here:
-
-https://aleksandarhaber.com/deep-q-networks-dqn-in-python-from-scratch-by-using-openai-gym-and-tensorflow-reinforcement-learning-tutorial/
-
-
-Author: Aleksandar Haber 
-Date: February 2023
 
 Tested on:
 
@@ -44,9 +30,6 @@ gamma=1
 # probability parameter for the epsilon-greedy approach
 epsilon=0.1
 # number of training episodes
-# NOTE HERE THAT AFTER CERTAIN NUMBERS OF EPISODES, WHEN THE PARAMTERS ARE LEARNED
-# THE EPISODE WILL BE LONG, AT THAT POINT YOU CAN STOP THE TRAINING PROCESS BY PRESSING CTRL+C
-# DO NOT WORRY, THE PARAMETERS WILL BE MEMORIZED
 numberEpisodes=1000
 
 # create an object
